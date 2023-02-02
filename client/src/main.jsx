72d5c0ac824be1df84944235fc20cb97e647589c
Jaydeep <jaydeep.dey03@gmail.com>
2023-02-02 22:42:45 +0530

    Updated folder structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ThirdwebProvider>
 )
