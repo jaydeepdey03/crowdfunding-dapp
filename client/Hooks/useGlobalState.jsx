28071d8325bd6bc76aea728d0a1542714811afce
Jaydeep <jaydeep.dey03@gmail.com>
2023-02-10 01:30:07 +0530

    removed nextjs

--- client/Hooks/useGlobalState.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { Context } from "../Context/Contractcontext"
-
-const useGlobalState = () => {
-    return useContext(Context)
-}
-
-
-export default useGlobalState
