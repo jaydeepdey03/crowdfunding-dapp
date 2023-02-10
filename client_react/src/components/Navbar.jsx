4f9e00404cae40642ef671d7014b575c36492a55
Jaydeep <jaydeep.dey03@gmail.com>
2023-02-10 09:35:07 +0530

    UI Fixes

--- client_react/src/components/Navbar.jsx
+++ client_react/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             {/* Responsive hamburger menu */}
             <div className='sm:hidden'>
                 <Link to="/profile">
-                    <div className={`w-[52px] h-[52px] rounded-full bg-[#2c2f32] sm:flex justify-center items-center cursor-pointer hidden`}>
+                    <div className={`w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer`}>
                         <img src={`/assets/thirdweb.png`} alt="user" className="w-[60%] h-[60%] object-contain" />
                     </div>
                 </Link>
